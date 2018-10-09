fb489cfef4db8bc28f22b3b0437492c8f152850b
Aanuragkumarsrivastava <42467453+Aanuragkumarsrivastava@users.noreply.github.com>
2018-10-09 16:10:20 +0530

    create main.java

---
+++ main.java
@@ -0,0 +1,24 @@
+
+//printing the even number between 1 to 100
+
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+  
+		for(int i=1;i<=100;i++)
+  //for loop running from number 1 to 100
+		{
+    
+			if(i%2==0)
+    //checking the number is divisible by 2 or not 
+			System.out.println(i);
+  //printing the number divisible by two 
+		}
+  
+	
+}
+
+}
